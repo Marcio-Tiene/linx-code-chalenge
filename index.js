65794f6a6b94766a54a5f16284510928b29879df
Marcio Tiene <marciorft@gmail.com>
2020-12-05 00:45:39 -0300

    adjusting first article and testing script

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function Test() {
+  const algorithmSection = document.getElementById('help-algorithm');
+  console.log(algorithmSection);
+  const testH1 = document.createElement('h1');
+  const testH1Text = document.createTextNode('hello linx');
 
+  testH1.appendChild(testH1Text);
+
+  algorithmSection.appendChild(testH1);
+}
+
+Test();
